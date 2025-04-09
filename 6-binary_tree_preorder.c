583c976e538225d0e2044cee0e3225de526304ff
md <md@outlook.com>
2025-04-09 23:01:02 +0000

    6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -13,13 +13,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree)
 		return;
 
-	if (!tree->left)
-	{	return;
-
-	if (!tree->right)
-		return;
-	}
-
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
