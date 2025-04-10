f67f91efa8c0f1b8ed3221c482e3e9309ed22708
md <md@outlook.com>
2025-04-10 06:05:04 +0000

    13-binary_tree_nodes.c

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes
+ * @tree: pointer to the root nodeof the tree
+ *
+ * Return: tree is NULL, the function must return 0
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (tree->left != NULL); 
+	{
+		if (tree->right != NULL);
+
+		return (1 + binary_tree_nodes(tree->left) +
+			binary_tree_nodes(tree->right));
+	}
+		return (binary_tree_nodes(tree->left) +
+			(binary_tree_nodes(tree->right)));
+}
