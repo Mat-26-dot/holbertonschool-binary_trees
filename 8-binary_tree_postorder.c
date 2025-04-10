1b35a4f9d5c0677581525ea04c70afb2770d39dc
md <md@outlook.com>
2025-04-10 00:51:07 +0000

    8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -3,9 +3,11 @@
 #include "binary_trees.h"
 
 /**
- * print_num - Prints a number
+ * binary_tree_postorder - binary tree using post-order traversal
+ * @tree: pointer to the root node
+ * @func: function to call for each node
  *
- * @n: Number to be printed
+ * Return: return tree or func if NULL
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
